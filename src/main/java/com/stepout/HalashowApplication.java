4bd304e19f1d2d11106c04eb4ab520150eec021d
mcdefender <493487386@qq.com>
2017-08-28 12:07:28 +0800

    test

--- src/main/java/com/stepout/HalashowApplication.java
+++ src/main/java/com/stepout/HalashowApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HalashowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HalashowApplication.class, args);
+		SpringApplication.run(HalashowApplication.class, args); 
+		
 	}
 }
  
